b26b184c105dd6568ff6e4b464dc0e80057614be
zwhy <125182767+wzxzwhy@users.noreply.github.com>
2025-04-30 19:43:00 +0800

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -18,7 +18,7 @@ install_xrayr() {
     bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
     # 修改 config.yml 配置文件
     echo "正在初始化 config.yml 文件..."
-    echo -e "Log:\n  Level: warning\n  AccessPath: ''\n  ErrorPath: ''\nDnsConfigPath: '/etc/XrayR/dns.json'\nRouteConfigPath: '/etc/XrayR/route.json'\nInboundConfigPath: '/etc/XrayR/custom_inbound.json'\nOutboundConfigPath: '/etc/XrayR/custom_outbound.json'\nConnectionConfig:\n  Handshake: 4\n  ConnIdle: 30\n  UplinkOnly: 2\n  DownlinkOnly: 4\n  BufferSize: 64\nNodes:" > $CONFIG_FILE
+    echo -e "Log:\n  Level: warning # Log level: none, error, warning, info, debug\n  AccessPath: # /etc/XrayR/access.Log\n  ErrorPath: # /etc/XrayR/error.log\nDnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help\nRouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help\nInboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help\nOutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help\nConnectionConfig:\n  Handshake: 4 # Handshake time limit, Second\n  ConnIdle: 30 # Connection idle time limit, Second\n  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second\n  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second\n  BufferSize: 64 # The internal cache size of each connection, kB\nNodes:" > $CONFIG_FILE
     echo "XrayR 安装并初始化配置文件完成。"
 }
 
@@ -50,74 +50,7 @@ add_node() {
     echo "正在添加节点..."
 
     # 将节点配置添加到config.yml
-    echo "
-    - PanelType: 'NewV2board' # Panel type: SSpanel, NewV2board, PMpanel, Proxypanel, V2RaySocks, GoV2Panel, BunPanel
-      ApiConfig:
-        ApiHost: 'https://xb.zwhy.cc'
-        ApiKey: 'lzi0V41No3lVqnX9sYiGcnycy'
-        NodeID: $node_id
-        NodeType: $node_type # Node type: V2ray, Vmess, Vless, Shadowsocks, Trojan, Shadowsocks-Plugin
-        Timeout: 30
-        EnableVless: $enable_vless
-        VlessFlow: 'xtls-rprx-vision'
-        SpeedLimit: 200
-        DeviceLimit: 0
-        RuleListPath: ''
-        DisableCustomConfig: false
-      ControllerConfig:
-        ListenIP: 0.0.0.0
-        SendIP: 0.0.0.0
-        UpdatePeriodic: 60
-        EnableDNS: false
-        DNSType: 'AsIs'
-        EnableProxyProtocol: false
-        AutoSpeedLimitConfig:
-          Limit: 0
-          WarnTimes: 0
-          LimitSpeed: 0
-          LimitDuration: 0
-        GlobalDeviceLimitConfig:
-          Enable: false
-          RedisNetwork: 'tcp'
-          RedisAddr: '127.0.0.1:6379'
-          RedisUsername: ''
-          RedisPassword: 'YOUR PASSWORD'
-          RedisDB: 0
-          Timeout: 5
-          Expiry: 60
-        EnableFallback: false
-        FallBackConfigs:
-          - SNI: ''
-            Alpn: ''
-            Path: ''
-            Dest: 80
-            ProxyProtocolVer: 0
-        DisableLocalREALITYConfig: $disable_local_reality
-        EnableREALITY: $enable_reality
-        REALITYConfigs:
-          Show: true
-          Dest: 'www.amazon.com:443'
-          ProxyProtocolVer: 0
-          ServerNames:
-            - 'www.amazon.com'
-          PrivateKey: 'YOUR_PRIVATE_KEY'
-          MinClientVer: ''
-          MaxClientVer: ''
-          MaxTimeDiff: 0
-          ShortIds:
-            - ''
-            - '0123456789abcdef'
-      CertConfig:
-        CertMode: 'dns'
-        CertDomain: 'node1.test.com'
-        CertFile: '/etc/XrayR/cert/node1.test.com.cert'
-        KeyFile: '/etc/XrayR/cert/node1.test.com.key'
-        Provider: 'alidns'
-        Email: 'test@me.com'
-        DNSEnv:
-          ALICLOUD_ACCESS_KEY: 'aaa'
-          ALICLOUD_SECRET_KEY: 'bbb'
-    " >> $CONFIG_FILE
+    echo "\n- PanelType: \"NewV2board\" # Panel type: SSpanel, NewV2board, PMpanel, Proxypanel, V2RaySocks, GoV2Panel, BunPanel\n  ApiConfig:\n    ApiHost: \"https://xb.zwhy.cc\"\n    ApiKey: \"lzi0V41No3lVqnX9sYiGcnycy\"\n    NodeID: $node_id\n    NodeType: $node_type # Node type: V2ray, Vmess, Vless, Shadowsocks, Trojan, Shadowsocks-Plugin\n    Timeout: 30 # Timeout for the api request\n    EnableVless: $enable_vless # Enable Vless for V2ray Type\n    VlessFlow: \"xtls-rprx-vision\" # Only support vless\n    SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable\n    DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable\n    RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file\n    DisableCustomConfig: false # disable custom config for sspanel\n  ControllerConfig:\n    ListenIP: 0.0.0.0 # IP address you want to listen\n    SendIP: 0.0.0.0 # IP address you want to send pacakage\n    UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.\n    EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well\n    DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy\n    EnableProxyProtocol: false # Only works for WebSocket and TCP\n    AutoSpeedLimitConfig:\n      Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)\n      WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.\n      LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)\n      LimitDuration: 0 # How many minutes will the limiting last (unit: minute)\n    GlobalDeviceLimitConfig:\n      Enable: false # Enable the global device limit of a user\n      RedisNetwork: tcp # Redis protocol, tcp or unix\n      RedisAddr: 127.0.0.1:6379 # Redis server address, or unix socket path\n      RedisUsername: # Redis username\n      RedisPassword: YOUR PASSWORD # Redis password\n      RedisDB: 0 # Redis DB\n      Timeout: 5 # Timeout for redis request\n      Expiry: 60 # Expiry time (second)\n    EnableFallback: false # Only support for Trojan and Vless\n    FallBackConfigs:  # Support multiple fallbacks\n      - SNI: # TLS SNI(Server Name Indication), Empty for any\n        Alpn: # Alpn, Empty for any\n        Path: # HTTP PATH, Empty for any\n        Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.\n        ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable\n    DisableLocalREALITYConfig: $disable_local_reality  # disable local reality config\n    EnableREALITY: $enable_reality # Enable REALITY\n    REALITYConfigs:\n      Show: true # Show REALITY debug\n      Dest: www.amazon.com:443 # Required, Same as fallback\n      ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable\n      ServerNames: # Required, list of available serverNames for the client, * wildcard is not supported at the moment.\n        - www.amazon.com\n      PrivateKey: YOUR_PRIVATE_KEY # Required, execute './XrayR x25519' to generate.\n      MinClientVer: # Optional, minimum version of Xray client, format is x.y.z.\n      MaxClientVer: # Optional, maximum version of Xray client, format is x.y.z.\n      MaxTimeDiff: 0 # Optional, maximum allowed time difference, unit is in milliseconds.\n      ShortIds: # Required, list of available shortIds for the client, can be used to differentiate between different clients.\n        - \"\"\n        - 0123456789abcdef\n  CertConfig:\n    CertMode: dns # Option about how to get certificate: none, file, http, tls, dns. Choose \"none\" will forcedly disable the tls config.\n    CertDomain: \"node1.test.com\" # Domain to cert\n    CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file\n    KeyFile: /etc/XrayR/cert/node1.test.com.key\n    Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/\n    Email: test@me.com\n    DNSEnv: # DNS ENV option used by DNS provider\n      ALICLOUD_ACCESS_KEY: aaa\n      ALICLOUD_SECRET_KEY: bbb\n" >> $CONFIG_FILE
 
     # 提示用户添加成功
     echo "节点已成功添加到 config.yml 文件中！"
