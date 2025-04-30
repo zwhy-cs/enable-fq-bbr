d205dfb5f3a3005b46c273406c5529dd2dabcc9a
zwhy <125182767+wzxzwhy@users.noreply.github.com>
2025-04-30 19:49:42 +0800

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -9,6 +9,7 @@ echo "1. 安装 XrayR"
 echo "2. 重启 XrayR"
 echo "3. 添加节点"
 echo "4. 退出"
+echo "5. 一键删除所有XrayR相关文件和配置"
 echo "--------------------------------------------------"
 read -p "请选择操作： " choice
 
@@ -50,12 +51,96 @@ add_node() {
     echo "正在添加节点..."
 
     # 将节点配置添加到config.yml
-    echo "\n- PanelType: \"NewV2board\" # Panel type: SSpanel, NewV2board, PMpanel, Proxypanel, V2RaySocks, GoV2Panel, BunPanel\n  ApiConfig:\n    ApiHost: \"https://xb.zwhy.cc\"\n    ApiKey: \"lzi0V41No3lVqnX9sYiGcnycy\"\n    NodeID: $node_id\n    NodeType: $node_type # Node type: V2ray, Vmess, Vless, Shadowsocks, Trojan, Shadowsocks-Plugin\n    Timeout: 30 # Timeout for the api request\n    EnableVless: $enable_vless # Enable Vless for V2ray Type\n    VlessFlow: \"xtls-rprx-vision\" # Only support vless\n    SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable\n    DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable\n    RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file\n    DisableCustomConfig: false # disable custom config for sspanel\n  ControllerConfig:\n    ListenIP: 0.0.0.0 # IP address you want to listen\n    SendIP: 0.0.0.0 # IP address you want to send pacakage\n    UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.\n    EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well\n    DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy\n    EnableProxyProtocol: false # Only works for WebSocket and TCP\n    AutoSpeedLimitConfig:\n      Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)\n      WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.\n      LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)\n      LimitDuration: 0 # How many minutes will the limiting last (unit: minute)\n    GlobalDeviceLimitConfig:\n      Enable: false # Enable the global device limit of a user\n      RedisNetwork: tcp # Redis protocol, tcp or unix\n      RedisAddr: 127.0.0.1:6379 # Redis server address, or unix socket path\n      RedisUsername: # Redis username\n      RedisPassword: YOUR PASSWORD # Redis password\n      RedisDB: 0 # Redis DB\n      Timeout: 5 # Timeout for redis request\n      Expiry: 60 # Expiry time (second)\n    EnableFallback: false # Only support for Trojan and Vless\n    FallBackConfigs:  # Support multiple fallbacks\n      - SNI: # TLS SNI(Server Name Indication), Empty for any\n        Alpn: # Alpn, Empty for any\n        Path: # HTTP PATH, Empty for any\n        Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.\n        ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable\n    DisableLocalREALITYConfig: $disable_local_reality  # disable local reality config\n    EnableREALITY: $enable_reality # Enable REALITY\n    REALITYConfigs:\n      Show: true # Show REALITY debug\n      Dest: www.amazon.com:443 # Required, Same as fallback\n      ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable\n      ServerNames: # Required, list of available serverNames for the client, * wildcard is not supported at the moment.\n        - www.amazon.com\n      PrivateKey: YOUR_PRIVATE_KEY # Required, execute './XrayR x25519' to generate.\n      MinClientVer: # Optional, minimum version of Xray client, format is x.y.z.\n      MaxClientVer: # Optional, maximum version of Xray client, format is x.y.z.\n      MaxTimeDiff: 0 # Optional, maximum allowed time difference, unit is in milliseconds.\n      ShortIds: # Required, list of available shortIds for the client, can be used to differentiate between different clients.\n        - \"\"\n        - 0123456789abcdef\n  CertConfig:\n    CertMode: dns # Option about how to get certificate: none, file, http, tls, dns. Choose \"none\" will forcedly disable the tls config.\n    CertDomain: \"node1.test.com\" # Domain to cert\n    CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file\n    KeyFile: /etc/XrayR/cert/node1.test.com.key\n    Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/\n    Email: test@me.com\n    DNSEnv: # DNS ENV option used by DNS provider\n      ALICLOUD_ACCESS_KEY: aaa\n      ALICLOUD_SECRET_KEY: bbb\n" >> $CONFIG_FILE
+    cat >> $CONFIG_FILE <<EOF
+  - PanelType: "NewV2board" # Panel type: SSpanel, NewV2board, PMpanel, Proxypanel, V2RaySocks, GoV2Panel, BunPanel
+    ApiConfig:
+      ApiHost: "https://xb.zwhy.cc"
+      ApiKey: "lzi0V41No3lVqnX9sYiGcnycy"
+      NodeID: $node_id
+      NodeType: $node_type # Node type: V2ray, Vmess, Vless, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: $enable_vless # Enable Vless for V2ray Type
+      VlessFlow: "xtls-rprx-vision" # Only support vless
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
+      DisableCustomConfig: false # disable custom config for sspanel
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      AutoSpeedLimitConfig:
+        Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)
+        WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
+        LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)
+        LimitDuration: 0 # How many minutes will the limiting last (unit: minute)
+      GlobalDeviceLimitConfig:
+        Enable: false # Enable the global device limit of a user
+        RedisNetwork: tcp # Redis protocol, tcp or unix
+        RedisAddr: 127.0.0.1:6379 # Redis server address, or unix socket path
+        RedisUsername: # Redis username
+        RedisPassword: YOUR PASSWORD # Redis password
+        RedisDB: 0 # Redis DB
+        Timeout: 5 # Timeout for redis request
+        Expiry: 60 # Expiry time (second)
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        - SNI: # TLS SNI(Server Name Indication), Empty for any
+          Alpn: # Alpn, Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable
+      DisableLocalREALITYConfig: $disable_local_reality  # disable local reality config
+      EnableREALITY: $enable_reality # Enable REALITY
+      REALITYConfigs:
+        Show: true # Show REALITY debug
+        Dest: www.amazon.com:443 # Required, Same as fallback
+        ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable
+        ServerNames: # Required, list of available serverNames for the client, * wildcard is not supported at the moment.
+          - www.amazon.com
+        PrivateKey: YOUR_PRIVATE_KEY # Required, execute './XrayR x25519' to generate.
+        MinClientVer: # Optional, minimum version of Xray client, format is x.y.z.
+        MaxClientVer: # Optional, maximum version of Xray client, format is x.y.z.
+        MaxTimeDiff: 0 # Optional, maximum allowed time difference, unit is in milliseconds.
+        ShortIds: # Required, list of available shortIds for the client, can be used to differentiate between different clients.
+          - ""
+          - 0123456789abcdef
+      CertConfig:
+        CertMode: dns # Option about how to get certificate: none, file, http, tls, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "node1.test.com" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+EOF
 
     # 提示用户添加成功
     echo "节点已成功添加到 config.yml 文件中！"
     # 重启 XrayR 使配置生效
     restart_xrayr
+}
+
+# 一键删除所有XrayR相关文件和配置
+remove_all_xrayr() {
+    echo "警告：即将删除所有XrayR相关文件和配置！"
+    read -p "确定要继续吗？(yes/no): " confirm
+    if [[ "$confirm" == "yes" ]]; then
+        systemctl stop XrayR 2>/dev/null
+        systemctl disable XrayR 2>/dev/null
+        rm -rf /etc/XrayR
+        rm -rf /usr/local/XrayR
+        rm -f /etc/systemd/system/XrayR.service
+        systemctl daemon-reload
+        echo "所有XrayR相关文件和配置已删除。"
+    else
+        echo "操作已取消。"
+    fi
 }
 
 # 根据用户选择执行相应的操作
@@ -72,6 +157,9 @@ case $choice in
     4)
         echo "退出脚本。"
         exit 0
+        ;;
+    5)
+        remove_all_xrayr
         ;;
     *)
         echo "无效选项，退出脚本。"
