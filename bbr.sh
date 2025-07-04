7ceb2b59e81ed6b58dc8dd2cbd42ebefd9893de2
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2025-07-04 18:31:29 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -142,6 +142,11 @@ fi
 cat <<EOF > /etc/sysctl.conf
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_wmem=4096 16384 20000000
+net.ipv4.tcp_rmem=4096 87380 20000000
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
 EOF
 
 # 使 sysctl 配置生效
