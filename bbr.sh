0709c53c03beedeff6d60b0d403613ea8702c239
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2025-09-04 10:56:03 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -31,7 +31,6 @@ net.ipv4.tcp_wmem = 4096 16384 50000000
 net.ipv4.tcp_rmem = 4096 87380 50000000
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1
 net.core.rmem_max = 50000000
 net.core.wmem_max = 50000000
 EOF
