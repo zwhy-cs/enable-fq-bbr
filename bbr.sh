282c779ffea1ee8362be7cf38f3bd340cd1f1c0f
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2025-10-18 21:53:59 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -31,8 +31,6 @@ net.ipv4.tcp_wmem = 4096 16384 50000000
 net.ipv4.tcp_rmem = 4096 87380 50000000
 net.core.rmem_max = 50000000
 net.core.wmem_max = 50000000
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
 EOF
 
 # 使 sysctl 配置生效
